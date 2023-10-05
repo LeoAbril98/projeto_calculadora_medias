84657b85969d80572ab1d68434468520030f7e7a
Leonardo Abril <leoabril98@gmail.com>
2023-10-04 21:28:01 -0300

    fix: corrigindo nome imagem

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="emoji celebrando" />';
-const imgReprovado = '<img src="./images/Reprovado.png" alt="emoji decepcionado" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="emoji decepcionado" />';
 const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
